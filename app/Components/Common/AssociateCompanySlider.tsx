3dd13d3b28bf0e870b902c98dba3794762017c37
varun <VarunSureshkumarPatel07@gmail.com>
2025-12-15 21:17:36 +0530

    chore: moving to word the production

--- app/Components/Common/AssociateCompanySlider.tsx
+++ app/Components/Common/AssociateCompanySlider.tsx
@@ -5,12 +5,12 @@ import "swiper/css";
 function AssociateCompanySlider() {
   const images = [
     { src: "/associates-logos/amazon.png", alt: "amazon" },
-
+    { src: "/associates-logos/orbit-ai.png", alt: "orbitrms" },
     { src: "/associates-logos/dribbble.png", alt: "dribbble" },
 
     { src: "/associates-logos/hubspot.png", alt: "hubspot" },
-
     { src: "/associates-logos/netflix.png", alt: "netflix" },
+    { src: "/associates-logos/orbitrms.png", alt: "orbitrms" },
 
     { src: "/associates-logos/notion.png", alt: "notion" },
 
@@ -26,12 +26,12 @@ function AssociateCompanySlider() {
 
   return (
     <div className="pb-5 overflow-hidden">
-      <div className="flex w-[100%] animate-marquee">
-        {Array?.from({ length: 5 })?.map(() =>
+      <div className="flex w-full animate-marquee">
+        {Array?.from({ length: 3 })?.map(() =>
           images.map((logo, i) => (
             <div className="flex items-center justify-center w-48 h-24" key={i}>
               <picture>
-                <img src={logo?.src} alt={logo?.alt} />
+                <img src={logo?.src} alt={logo?.alt} width={128} height={48} />
               </picture>
             </div>
           ))
