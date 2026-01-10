6065d93dce6be026d2a6e9a138439bafbd41a4f8
varun <VarunSureshkumarPatel07@gmail.com>
2026-01-10 23:59:38 +0530

    chore: updated the code with the new chnage

--- app/Constant/OurTeamIntro.ts
+++ app/Constant/OurTeamIntro.ts
@@ -6,20 +6,23 @@ export const OurTeamIntroSection: TeamInfoInterface[] = [
     name: "Jasmin Rajput",
     designation: "Founder",
     linkedinUrl: "https://www.linkedin.com/in/jasmin-rajput-05b367395/",
-    description: "10+ years of experience in digital marketing. Expertise in SEO, PPC, and content strategy",
+    description:
+      "Jasmin Rajput is the Founder & CEO of Thinkarq, leading the company with the vision “Think, Build & Disrupt.",
   },
   {
     imagePath: "/team/vaibhav-rajput.webp",
     name: "Vaibhav Rajput",
     designation: "CEO",
     linkedinUrl: "https://www.linkedin.com/in/vaibhav-rajput-1722bv",
-    description: "5+ years of experience in digital marketing. Expertise in SEO, PPC, and content strategy",
+    description:
+      "Vaibhav Rajput leads Thinkarq’s technology strategy, driving innovation through scalable digital solutions. He is passionate about building robust systems that help businesses grow in a rapidly evolving tech landscape.",
   },
-    {
+  {
     imagePath: "/team/aarsh-shukla.png",
     name: "Aarsh Shukla",
     designation: "CSO",
     linkedinUrl: "https://www.linkedin.com/in/aarsh-shukla-0a2638190/",
-    description: "Aarsh Shukla is the CSO who turns cybersecurity into strength. With 5+ years of experience, he fortifies everything we build.",
+    description:
+      "Aarsh Shukla is the Chief Security Officer (CSO) at Thinkarq, leading the company’s cybersecurity strategy and risk management initiatives. He ensures robust security frameworks that protect digital assets, data, and client trust across all operations.",
   },
 ];
